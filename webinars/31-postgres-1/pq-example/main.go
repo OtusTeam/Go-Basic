616940e41deb013357002ab43fc89fb02b2e7213
evgenyfedoseev <ef7@mail.ru>
2025-04-28 19:21:55 +0300

    postgres-1: wip

--- webinars/31-postgres-1/pq-example/main.go
+++ webinars/31-postgres-1/pq-example/main.go
@@ -2,13 +2,42 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
 	"log"
 )
 
 func main() {
-	_, err := sql.Open("postgres", "postgresql://localhost:5432/otus-go-basic")
+	db, err := sql.Open("postgres", "postgresql://user:password@localhost:5432/otus-go-basic?sslmode=disable")
 	if err != nil {
 		log.Fatalf("Couldn't open db connection, err: %s", err.Error())
+	}
+
+	insertValues(db)
+	selectValues(db)
+}
+
+func insertValues(db *sql.DB) {
+	_, err := db.Exec("insert into users(name) values('John'), ('Patric')")
+	if err != nil {
+		log.Fatalf("Couldn't insert users, err: %s", err.Error())
+	}
+
+	_, err = db.Exec("insert into books(name, user_id) values('Peace and war', 1), ('Idiot', 2)")
+	if err != nil {
+		log.Fatalf("Couldn't insert books, err: %s", err.Error())
+	}
+}
+
+func selectValues(db *sql.DB) {
+	rows, err := db.Query("select id, name from users")
+	if err != nil {
+		log.Fatalf("Couldn't insert users, err: %s", err.Error())
+	}
+	fmt.Println(rows)
+
+	rows, err = db.Query("select id, name, user_id from books")
+	if err != nil {
+		log.Fatalf("Couldn't insert books, err: %s", err.Error())
 	}
 }
